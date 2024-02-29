0ba04da7a4c3084dcee604f76404c5feac63d593
Tomislav Tipurić <tomislavt@hotmail.com>
2024-02-29 14:15:34 +0000

    Added a Go sample and updated readme file.

--- README.md
+++ README.md
@@ -1,29 +1,91 @@
-# helloworld
+# Code100 Competition - Go Sample
 
-Hello World for Golang
+This codebase contains everything you need to complete coding challenges of the Code100 competition in Go.
+The code example of communicating with the API is already present in the `main.go`.
 
-## Simple Command
+If you need additional information regarding the API you may find it in the rest of this document.
 
-Run golang program
+### Running the project
 
-```bash
+Open the terminal and run:
+```sh
 go run main.go
 ```
 
-Testing
-
-```bash
-go test
+## API Base URL
+```
+https://challenger.code100.dev
 ```
 
-Build binary
-
-```bash
-go build
+## Swagger Documentation
 ```
+https://challenger.code100.dev/doc/
+```
+## Login
 
-Install binary
+In order to request the coding puzzle or submit the solution, you first need to retrieve the JSON Web Token by logging into the API:
 
-```bash
-go install
+### Request
+```
+POST /login
+```
+### Body
+```
+{
+  "email": <email here>,
+  "password": <password here>
+}
+```
+### Response
+```
+{
+   ...
+   "token": <JWT token>
+   ...
+}
+```
+The token given in this response needs to be supplied for the Request Puzzle and Submit Solution requests.
+
+## Request Puzzle
+
+### Request
+```
+GET /getpuzzle
+```
+### Headers
+```
+Authorization: Bearer <JWT token>
+```
+### Response
+```
+{
+  "name": <name of the puzzle>,
+  "input": <coding challenge>
+}
+```
+(coding challenge depends from round to round)
+
+## Submit Solution
+
+### Request
+```
+POST /postanswer
+```
+### Headers
+```
+Authorization: Bearer <JWT token>
+```
+### Body
+```
+{
+  "answer": <your solution to the posted puzzle>
+}
+```
+### Response
+
+There is a 15 seconds delay for you to receive the answer for this request.
+```
+{
+  "message": <information if solution was correct or not>,
+}
 ```
--- main.go
+++ main.go
@@ -1,11 +1,130 @@
 package main
 
-import "fmt"
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+)
 
 func helloworld() string {
 	return "Hello World!!"
 }
 
 func main() {
-	fmt.Println(helloworld())
+
+	email := "EMAIL-OR-USERNAME-HERE" // replace with email or username given to you
+	password := "PASSWORD-HERE"       // replace with password given to you
+
+	// Step 1: Login to the API endpoint
+	credentials, err := json.Marshal(map[string]string{
+		"email":    email,
+		"password": password,
+	})
+	if err != nil {
+		fmt.Println("Error: ", err)
+	}
+
+	response, err := http.Post("https://challenger.code100.dev/login", "application/json", bytes.NewReader(credentials))
+	if err != nil {
+		fmt.Println("Error Login: ", err)
+		return
+	}
+	defer response.Body.Close()
+
+	if response.StatusCode != 200 {
+		fmt.Println("Error: ", response.Status)
+		return
+	}
+
+	var result map[string]interface{}
+	json.NewDecoder(response.Body).Decode(&result)
+	token := result["token"].(string)
+
+	// Step 2: Call Authenticated Endpoint /testauthroute
+	req, err := http.NewRequest("GET", "https://challenger.code100.dev/testauthroute", nil)
+	if err != nil {
+		fmt.Println("Error: ", err)
+		return
+	}
+	req.Header.Set("Authorization", "Bearer "+token)
+	req.Header.Set("Content-Type", "application/json")
+	client := &http.Client{}
+	response, err = client.Do(req)
+	if err != nil {
+		fmt.Println("Error: ", err)
+		return
+	}
+	defer response.Body.Close()
+
+	body, err := io.ReadAll(response.Body)
+	if err != nil {
+		fmt.Println("Error: ", err)
+		return
+	}
+	sb := string(body)
+	fmt.Println("Response: ", sb)
+
+	// Step 3: Get the puzzle
+	req, err = http.NewRequest("GET", "https://challenger.code100.dev/getpuzzle", nil)
+	if err != nil {
+		fmt.Println("Error: ", err)
+		return
+	}
+	req.Header.Set("Authorization", "Bearer "+token)
+	req.Header.Set("Content-Type", "application/json")
+	client = &http.Client{}
+	response, err = client.Do(req)
+	if err != nil {
+		fmt.Println("Error: ", err)
+		return
+	}
+	defer response.Body.Close()
+
+	body, err = io.ReadAll(response.Body)
+	if err != nil {
+		fmt.Println("Error: ", err)
+		return
+	}
+	sb = string(body)
+	fmt.Println("Puzzle: ", sb)
+
+	// Step 4: Solve the puzzle
+
+	////////////////////////////
+	////// YOUR CODE HERE //////
+	////////////////////////////
+
+	answer, err := json.Marshal(map[string]string{
+		"answer": "Answer in the required format",
+	})
+	if err != nil {
+		fmt.Println("Error: ", err)
+	}
+
+	// Step 5: Submit the solution
+	req, err = http.NewRequest("POST", "https://challenger.code100.dev/postanswer", bytes.NewReader(answer))
+	if err != nil {
+		fmt.Println("Error: ", err)
+		return
+	}
+	req.Header.Set("Authorization", "Bearer "+token)
+	req.Header.Set("Content-Type", "application/json")
+	client = &http.Client{}
+	response, err = client.Do(req)
+	if err != nil {
+		fmt.Println("Error: ", err)
+		return
+	}
+	defer response.Body.Close()
+
+	body, err = io.ReadAll(response.Body)
+	if err != nil {
+		fmt.Println("Error: ", err)
+		return
+	}
+	sb = string(body)
+	fmt.Println("Response: ", sb)
+
 }
